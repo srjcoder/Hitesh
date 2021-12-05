d976a1816aa2f2948f9e488c7f9f530caff137d3
sudharsanraju N S <sudharsanraju.s@atmecs.com>
2021-12-05 09:46:53 -0800

    tut 7 8 9 10 videos and other

---
+++ m_assigns/calcu_w_fuc/arith/arith.go
@@ -0,0 +1,16 @@
+package arith
+
+import "fmt"
+
+func init() {
+	fmt.Println("calculations with arithmetics")
+
+}
+
+func Add(a int, b int) int {
+	return a + b
+}
+
+func Sub(a int, b int) int {
+	return a - b
+}
